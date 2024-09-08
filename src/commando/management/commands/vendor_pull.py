c9410d97f051e63d6888c18369dda458b0e25e9b
amansr18 <amansr1802@gmail.com>
2024-09-08 21:31:13 +0530

    Added Static_files configurations

---
+++ src/commando/management/commands/vendor_pull.py
@@ -0,0 +1,37 @@
+import helpers
+
+from typing import Any
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
+
+VENDOR_STATIC_FILES = {
+    "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
+    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+}
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any):
+        self.stdout.write("Downloading vendor static files")
+        completed_urls = []
+        
+        for name, urls in VENDOR_STATIC_FILES.items():
+            out_path = STATICFILES_VENDOR_DIR / name
+            dl_success = helpers.download_to_local(urls, out_path)
+            if dl_success:
+                completed_urls.append(urls)
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to download {urls}")
+                )
+
+        if set(completed_urls) == set(VENDOR_STATIC_FILES.values()):
+            self.stdout.write(
+                self.style.SUCCESS("Successfully downloaded all vendor static files")
+            )
+        else:
+            self.stdout.write(
+                self.style.ERROR("Some files were not downloaded")
+            )
--- src/saas_base/settings.py
+++ src/saas_base/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     #my apps
     "visits",
+    "commando",
 ]
 
 MIDDLEWARE = [
@@ -141,6 +142,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
