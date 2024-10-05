c95e1230a76a478010d8ef7fa41b2994556f4442
amansr18 <amansr1802@gmail.com>
2024-10-05 18:40:53 +0530

    added error handiling in checkout

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -18,44 +18,34 @@ def product_price_redirect_view(request, price_id=None, *args, **kwargs):
     request.session['checkout_subscription_price_id'] = price_id
     return redirect('stripe-checkout-start')
 
-@login_required
-def checkout_redirect_view(request):
-    checkout_subscription_price_id = request.session.get('checkout_subscription_price_id')
-    try:
-        obj = SubscriptionPrice.objects.get(id=checkout_subscription_price_id)
-    except:
-        obj = None
+# Use this one for the checkout view
+# @login_required
+# def checkout_redirect_view(request):
+#     checkout_subscription_price_id = request.session.get('checkout_subscription_price_id')
+#     try:
+#         obj = SubscriptionPrice.objects.get(id=checkout_subscription_price_id)
+#     except:
+#         obj = None
 
-    if checkout_subscription_price_id is None or obj is None:
-        # return redirect('pricing') use this one for your project
-        return render(request, '404.html', status=404)
-    customer_stripe_id = request.user.customer.stripe_id
-    success_url_path = reverse("stripe-checkout-end")
-    pricing_url_path = reverse("pricing")
-    success_url = f"{BASE_URL}{success_url_path}"
-    cancel_url = f"{BASE_URL}{pricing_url_path}"
-    price_stripe_id = obj.stripe_id
-    # url = helpers.billing.create_checkout_session(
-    #     customer_stripe_id,
-    #     success_url= success_url,
-    #     cancel_url= cancel_url,
-    #     price_stripe_id=price_stripe_id,
-    #     raw=False
-    # )
+#     if checkout_subscription_price_id is None or obj is None:
+#         return redirect('pricing')
+#     customer_stripe_id = request.user.customer.stripe_id
+#     success_url_path = reverse("stripe-checkout-end")
+#     pricing_url_path = reverse("pricing")
+#     success_url = f"{BASE_URL}{success_url_path}"
+#     cancel_url = f"{BASE_URL}{pricing_url_path}"
+#     price_stripe_id = obj.stripe_id
+#     url = helpers.billing.create_checkout_session(
+#         customer_stripe_id,
+#         success_url= success_url,
+#         cancel_url= cancel_url,
+#         price_stripe_id=price_stripe_id,
+#         raw=False
+#     )
 
-    try:
-        url = helpers.billing.create_checkout_session(
-            customer_stripe_id,
-            success_url=success_url,
-            cancel_url=cancel_url,
-            price_stripe_id=price_stripe_id,
-            raw=False
-        )
-    except PermissionError as e:
-        # Handle PermissionError and render custom 404
-        
-        return render(request, '404.html', status=404)
-    
+#     return redirect(url)
+
+def render_checkout_error_page(request):
     return render(request, '404.html', status=404)
 
 def checkout_finalize_view(request):
